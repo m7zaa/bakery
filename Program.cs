d9d7b84acb99601748e0efdd2d39e935e4b5e472
Micah Zaayer <micahzaayer@gmail.com>
2019-09-27 08:39:41 -0700

    Add readme and ui console lines.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,20 @@ namespace Bakery
     {
         public static void Main()
         {
+            Console.WriteLine("Welcome to Amy's Baking Co! Today, we have fresh sourdough loaves for $6 and our pastery of the day for $2. Would you like to buy anything? Y/N");
+            string PurchaseYN = Console.ReadLine();
+            if (PurchaseYN == "Y" || PurchaseYN == "y")
+            {
+                Console.WriteLine("How many loaves of sourdough would you like?");
+                int quantityLoaves = int.Parse(Console.ReadLine());
+                Console.WriteLine("How many pasteries would you like?");
+                int quantityPasteries = int.Parse(Console.ReadLine());            
+            }
 
+            else 
+            {
+                Console.WriteLine("Thanks for stopping by. I hope you have a great rest of your day!");
+            }
         }
 
        

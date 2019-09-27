719139ccf86fe1b09579e4fae17154973b7ecbb2
Micah Zaayer <micahzaayer@gmail.com>
2019-09-27 09:39:57 -0700

    Add variables to backend classes.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Bakery
     {
         public static void Main()
         {
-            Console.WriteLine("Welcome to Amy's Baking Co! Today, we have fresh sourdough loaves for $6 and our pastery of the day for $2. Would you like to buy anything? Y/N");
+            Console.WriteLine("Welcome to Amy's Baking Co! Today, we have fresh sourdough loaves for $5 (Buy 2, get 1 free) and our pastery of the day for $2/each or 3 for $5. Would you like to buy anything? Y/N");
             string PurchaseYN = Console.ReadLine();
             if (PurchaseYN == "Y" || PurchaseYN == "y")
             {
@@ -17,10 +17,9 @@ namespace Bakery
                 Console.WriteLine("How many pasteries would you like?");
                 int quantityPasteries = int.Parse(Console.ReadLine());            
             }
-
             else 
             {
-                Console.WriteLine("Thanks for stopping by. I hope you have a great rest of your day!");
+                Console.WriteLine("No problem. Thanks for stopping by. I hope you have a great rest of your day!");
             }
         }
 
